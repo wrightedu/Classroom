140fce4d1eff3ea8c4905ff311bae0b82ac2dab2
kalli koppin <kkoppin001@gmail.com>
2026-06-02 16:23:36 -0400

    now it can tell you what your role is with a hardcoded org

--- git_classroom.sh
+++ git_classroom.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-ORGANIZATION="WSU-kkoppin"
+ORGANIZATION="wrightedu"
 
 # Get the authenticated username
 
 USERNAME=$(gh api user --jq '.login')
 
 echo "Authenticated as $USERNAME"
+
+ROLE=$(gh api "/orgs/$ORGANIZATION/memberships/$USERNAME" --jq '.role' 2>/dev/null)
+
+echo "Authenticated user, $USERNAME, has the role of $ROLE in $ORGANIZATION"
